3be09fdd41faf169125485e564c2017260ed47e4
Bene <benedikt.bessen@gmx.de>
2015-06-13 14:18:22 +0200

    add transform inspector editor window

--- Assets/Scripts/Editor/Inspectors/TransformInspector.cs
+++ Assets/Scripts/Editor/Inspectors/TransformInspector.cs
@@ -1,55 +1,159 @@
-﻿// Reverse engineered UnityEditor.TransformInspector
-
+﻿// Alternative version, with redundant code removed
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
 [CustomEditor(typeof(Transform))]
-public class TransformInspector : Editor {
-	
-	private bool firstSet;
-	private Vector3 rotation;
-	private Quaternion oldQuaternion;
-	
-	
-	public TransformInspector ()
+public class TransformInspector : Editor
+{
+	private Transform follower;
+	private TransformSetup positionSetup = new TransformSetup();
+	private TransformSetup rotationSetup  = new TransformSetup();
+	private TransformSetup scaleSetup  = new TransformSetup();
+
+	private struct TransformSetup
 	{
-		this.firstSet = true;
+		public bool foldout;
+		public Transform target;
 	}
-	
-	
-	public override void OnInspectorGUI ()
+	public override void OnInspectorGUI()
 	{
+		
+		Transform t = (Transform)target;
+		
+		// Replicate the standard transform inspector gui
 		EditorGUIUtility.LookLikeControls();
-		Transform t = (Transform)this.target;
 		EditorGUI.indentLevel = 0;
-		if (!this.firstSet)
+
+		Vector3 position = PositionField (t);
+		Vector3 eulerAngles = RotationField (t);
+		Vector3 scale = ScaleField (t);
+
+		string buttonName = "Reset";
+		GUI.SetNextControlName(buttonName);
+		if(GUILayout.Button(buttonName))
 		{
-			if (this.oldQuaternion != t.localRotation)
-			{
-				this.firstSet = false;
-				this.rotation = t.localEulerAngles;
-				this.oldQuaternion = t.localRotation;
-			}
+			GUI.FocusControl(buttonName);
+			positionSetup = new TransformSetup();
+			rotationSetup  = new TransformSetup();
+			scaleSetup  = new TransformSetup();
+			position = Vector3.zero;
+			eulerAngles = Vector3.zero;
+			scale = Vector3.one;
 		}
-		Vector3 position = EditorGUILayout.Vector3Field("Position", t.localPosition);
-		this.rotation = EditorGUILayout.Vector3Field("Rotation", this.rotation);
-		Vector3 scale = EditorGUILayout.Vector3Field("Scale", t.localScale);
-		EditorGUILayout.TextArea("Test");
+
+		EditorGUIUtility.LookLikeInspector();
+		
 		if (GUI.changed)
 		{
 			Undo.RegisterUndo(t, "Transform Change");
-			this.rotation = this.FixIfNaN(this.rotation);
-			t.localPosition = this.FixIfNaN(position);
-			t.localEulerAngles = this.rotation;
-			this.oldQuaternion = t.localRotation;
-			t.localScale = this.FixIfNaN(scale);
+			
+			t.localPosition = FixIfNaN(position);
+			t.localEulerAngles = FixIfNaN(eulerAngles);
+			t.localScale = FixIfNaN(scale);
 		}
-		EditorGUIUtility.LookLikeInspector();
+	}
+
+	private Vector3 PositionField (Transform t)
+	{	
+		Vector3 position = t.localPosition;
+		EditorGUILayout.BeginHorizontal ();
+		positionSetup.foldout = EditorGUILayout.Foldout(positionSetup.foldout,"Position");
+		position = EditorGUILayout.Vector3Field ("", position);
+		EditorGUILayout.EndHorizontal ();
+
+		if(positionSetup.foldout)
+		{
+			string name = "Reset";
+			GUI.SetNextControlName(name);
+			EditorGUILayout.BeginHorizontal ();
+			if (GUILayout.Button (name, ButtonStyle)) 
+			{
+				GUI.FocusControl(name);
+				position = Vector3.zero;
+				positionSetup.target = null;
+			}
+			
+			positionSetup.target = (Transform) EditorGUI.ObjectField(EditorGUILayout.GetControlRect(), "", positionSetup.target, typeof(Transform));
+			EditorGUILayout.EndHorizontal ();
+		}
+		if(positionSetup.target)
+		{
+			position = positionSetup.target.position;
+		}
+		return position;
+	}
+
+	private Vector3 RotationField (Transform t)
+	{
+		EditorGUILayout.BeginHorizontal ();
+		rotationSetup.foldout = EditorGUILayout.Foldout(rotationSetup.foldout,"Rotation");
+		Vector3 eulerAngles = EditorGUILayout.Vector3Field ("", t.localEulerAngles);
+		EditorGUILayout.EndHorizontal ();
+		
+		if(rotationSetup.foldout)
+		{
+			string name = "Reset";
+			GUI.SetNextControlName(name);
+			EditorGUILayout.BeginHorizontal ();
+			if (GUILayout.Button (name, ButtonStyle)) 
+			{
+				GUI.FocusControl(name);
+				eulerAngles = Vector3.zero;
+				rotationSetup.target = null;
+			}
+			
+			rotationSetup.target = (Transform) EditorGUI.ObjectField(EditorGUILayout.GetControlRect(), "", rotationSetup.target, typeof(Transform));
+			EditorGUILayout.EndHorizontal ();
+		}
+		if(rotationSetup.target)
+		{
+			eulerAngles = rotationSetup.target.localRotation.eulerAngles;
+		}
+		return eulerAngles;
+	}
+
+	private Vector3 ScaleField (Transform t)
+	{
+		EditorGUILayout.BeginHorizontal ();
+		scaleSetup.foldout = EditorGUILayout.Foldout(scaleSetup.foldout,"Scale");
+		Vector3 scale = EditorGUILayout.Vector3Field ("", t.localScale);
+		EditorGUILayout.EndHorizontal ();
+		
+		if(scaleSetup.foldout)
+		{
+			string name = "Reset";
+			GUI.SetNextControlName(name);
+			EditorGUILayout.BeginHorizontal ();
+			if (GUILayout.Button ("Reset", ButtonStyle)) 
+			{
+				GUI.FocusControl(name);
+				scale = Vector3.one;
+				scaleSetup.target = null;
+			}
+			
+			scaleSetup.target = (Transform) EditorGUI.ObjectField(EditorGUILayout.GetControlRect(), "", scaleSetup.target, typeof(Transform));
+			EditorGUILayout.EndHorizontal ();
+		}
+		if(scaleSetup.target)
+		{
+			scale = scaleSetup.target.localScale;
+		}
+
+		return scale;
+	}
+
+	private GUIStyle ButtonStyle
+	{
+		get
+		{
+			GUIStyle style = new GUIStyle(EditorStyles.miniButton);
+			style.fixedWidth = 50f;
+			return style;
+		}
 	}
 	
-	
-	private Vector3 FixIfNaN (Vector3 v)
+	private Vector3 FixIfNaN(Vector3 v)
 	{
 		if (float.IsNaN(v.x))
 		{
@@ -67,4 +171,3 @@ public class TransformInspector : Editor {
 	}
 	
 }
-
