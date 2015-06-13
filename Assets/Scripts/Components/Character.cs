5048f53f100fba780630f35fd5776174c05ec513
Bene <benedikt.bessen@gmx.de>
2015-06-14 01:23:04 +0200

    Add controller input and create basic control

---
+++ Assets/Scripts/Components/Character.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour 
+{
+	[SerializeField]
+	private float rotateSpeed = 200.0f;
+	[SerializeField]
+	private float moveSpeed = 600.0f;
+
+	private InputManager input;
+	private Animator animator;
+	private Rigidbody rigidbody;
+
+	private Vector3 movement;
+	private Vector3 moveDirection;
+	private float force;
+
+	private void Awake()
+	{
+		input = GetComponent<InputManager>();
+		animator = GetComponentInChildren<Animator>();
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	private void Start()
+	{
+
+	}
+
+	protected virtual void FixedUpdate()
+	{
+		float speed = Mathf.Clamp01( Mathf.Abs(input.Horizontal) + Mathf.Abs(input.Vertical));
+		Vector3 direction = transform.TransformDirection(Vector3.forward);
+
+		force = Mathf.Lerp(force,speed,Time.deltaTime);
+
+		animator.SetFloat("movement",force);
+
+		speed = force * Time.deltaTime * moveSpeed;
+		direction *= speed;
+		direction.y = rigidbody.velocity.y;
+	
+		rigidbody.velocity = direction;
+	}
+	
+	protected virtual void Update()
+	{
+		Vector3 moveDirection = new Vector3(input.Horizontal,0,input.Vertical);
+		if (moveDirection != Vector3.zero)
+		{
+			movement = Vector3.RotateTowards(movement, moveDirection, rotateSpeed * Mathf.Deg2Rad * Time.deltaTime, 1000); //Direction
+			
+			movement.Normalize();
+			
+			transform.rotation = Quaternion.LookRotation(movement);
+		}
+	}
+}
---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using XInputDotNetPure;
+
+public class InputManager : MonoBehaviour
+{
+	[SerializeField]
+	private PlayerIndex playerIndex;
+	private GamePadState state;
+	private GamePadState prevState;
+
+	private void Start()
+	{
+		state = GamePad.GetState(playerIndex);
+	}
+
+	private void Update()
+	{
+		prevState = state;
+		state = GamePad.GetState(playerIndex);
+	}
+
+	public bool AcceptDown
+	{
+		get
+		{
+			return prevState.Buttons.A == ButtonState.Released && Accept;
+		}
+	}
+
+	public bool Accept
+	{
+		get
+		{
+			return	state.Buttons.A == ButtonState.Pressed;
+		}
+	}
+
+	public bool PlayDown
+	{
+		get
+		{
+			return prevState.Buttons.Start == ButtonState.Released && Play;
+		}
+	}
+	
+	public bool Play
+	{
+		get
+		{
+			return	state.Buttons.Start == ButtonState.Pressed;
+		}
+	}
+
+	public bool Pause
+	{
+		get
+		{
+			return	state.Buttons.Back == ButtonState.Pressed;
+		}
+	}
+
+	
+	public bool Cancel
+	{
+		get
+		{
+			return	state.Buttons.B == ButtonState.Pressed;
+		}
+	}
+
+	public bool IsActive
+	{
+		get
+		{
+			return	state.IsConnected;
+		}
+	}
+
+	public float Horizontal
+	{
+		get
+		{
+			return Input.GetAxis("Horizontal") + state.ThumbSticks.Left.X;
+		}
+	}
+
+	public float Vertical
+	{
+		get
+		{
+			return Input.GetAxis("Vertical") + state.ThumbSticks.Left.Y;
+		}
+	}
+
+	public float Jump
+	{
+		get
+		{
+			return Convert.ToInt32( state.Buttons.A == ButtonState.Pressed);
+		}
+	}
+
+	public bool Action
+	{
+		get
+		{
+			return Input.GetKey(KeyCode.E);
+		}
+	}
+
+	public void StartVibration(Vector2 direction,float duration)
+	{
+		StartCoroutine(Vibration(direction,duration));
+	}
+
+	private IEnumerator Vibration(Vector2 direction,float duration)
+	{
+		GamePad.SetVibration(playerIndex,direction.x, direction.y);
+		yield return new WaitForSeconds(duration);
+		GamePad.SetVibration(playerIndex,0,0);
+	}
+
+	public void StopVibration()
+	{
+		StopCoroutine("Vibration");
+	}
+}
